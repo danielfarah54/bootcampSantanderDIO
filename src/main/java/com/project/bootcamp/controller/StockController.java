be06d8b11c3fa55e65e2eb903cdc21ed11666d20
Daniel Farah <danielfarah54@gmail.com>
2021-06-27 18:29:05 -0300

    StockController: save method

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -1,5 +1,7 @@
 package com.project.bootcamp.controller;
-// import ;
+
+import com.project.bootcamp.model.dto.StockDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/stock")
 public class StockController {
     
+    public ResponseEntity<StockDTO> save()
 }
